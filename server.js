89c6500905c70bbc12fbba5d787e04e5d5c5d0c0
Taiwo Hassan Yusuf <teehazzan@gmail.com>
2020-05-08 22:52:36 +0100

    set for deployment finally

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ const serveStatic = require("serve-static")
 const path = require('path');
 app = express();
 app.use(serveStatic(path.join(__dirname, 'dist')));
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 3000;
 
 app.listen(port);
